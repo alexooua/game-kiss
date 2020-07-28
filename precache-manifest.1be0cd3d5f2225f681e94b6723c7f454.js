c4c45911b3ea2f9419ee0836f09aa1f8c7ace3f2
alex <alextotalk@gmail.com>
2020-07-28 11:32:18 +0300

    Updates

---
+++ precache-manifest.1be0cd3d5f2225f681e94b6723c7f454.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030a869e117b4e9d060b48497c3d64c9",
+    "url": "/game-The-way-for-the-front-end-samurai/index.html"
+  },
+  {
+    "revision": "989ec2775b86701a27e9",
+    "url": "/game-The-way-for-the-front-end-samurai/static/css/main.8fca44eb.chunk.css"
+  },
+  {
+    "revision": "72edabc957a014d5c3fe",
+    "url": "/game-The-way-for-the-front-end-samurai/static/js/2.5f3d6d2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-The-way-for-the-front-end-samurai/static/js/2.5f3d6d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989ec2775b86701a27e9",
+    "url": "/game-The-way-for-the-front-end-samurai/static/js/main.e07fbd8a.chunk.js"
+  },
+  {
+    "revision": "73764bc01954fd4b4181",
+    "url": "/game-The-way-for-the-front-end-samurai/static/js/runtime-main.2dce7fe2.js"
+  },
+  {
+    "revision": "19f149289b8c331bfb36bdc8b8efeda9",
+    "url": "/game-The-way-for-the-front-end-samurai/static/media/Basic.19f14928.cur"
+  },
+  {
+    "revision": "9da22213aa847bc9d5a94be6660f4f6e",
+    "url": "/game-The-way-for-the-front-end-samurai/static/media/Cy-BugProfile.9da22213.png"
+  },
+  {
+    "revision": "0ac3dd85687cfa5020e24413a4b45598",
+    "url": "/game-The-way-for-the-front-end-samurai/static/media/sword_swing_fast_whoo_003.0ac3dd85.mp3"
+  },
+  {
+    "revision": "2a5d7853701a01058def45bf7cc37bf0",
+    "url": "/game-The-way-for-the-front-end-samurai/static/media/zaglavnaja-tema-mortal-kombat-8-bit.2a5d7853.mp3"
+  }
+]);
